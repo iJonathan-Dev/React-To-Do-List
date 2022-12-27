276f441c4a3b97a238e507a0b7b656d1df572983
iJonathan-Dev <ignatius.jonathan@outlook.com>
2022-12-27 23:55:17 +0700

    Add To Do Form Entry
    Form to handle new to do list

---
+++ src/Component/AddToDoForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { DBUrl } from "./DBUrl";
+
+const AddToDoForm = ({ fetchData }) => {
+  const [newToDoListValue, setNewTodoListValue] = useState("");
+
+  const addToDo = (e, newToDoListValue) => {
+    e.preventDefault();
+
+    const toDo = { title: newToDoListValue, status: "incomplete" };
+
+    fetch(DBUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(toDo),
+    }).then(() => fetchData());
+  };
+
+  return (
+    <form onSubmit={(e) => addToDo(e, newToDoListValue)}>
+      <label htmlFor="ToDo-List">Add To Do List</label>
+      <input type="text" id="ToDo-List" placeholder="Type here..." value={newToDoListValue} onChange={(e) => setNewTodoListValue(e.target.value)} required />
+      <button id="add-btn" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default AddToDoForm;
