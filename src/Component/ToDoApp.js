717ce7566f4af22d5633b65e3bc556c7182532d3
iJonathan-Dev <ignatius.jonathan@outlook.com>
2022-12-27 23:56:11 +0700

    Main App Container

---
+++ src/Component/ToDoApp.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import AddToDoForm from "./AddToDoForm";
+import ListView from "./ListView";
+import { DBUrl } from "./DBUrl";
+
+const ToDoApp = () => {
+  const [toDoListData, setTodoListData] = useState("");
+
+  const fetchData = () => {
+    fetch(DBUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Failed to Fetch Data!");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        //Filter Incomplete Data
+        const result = data.filter((item) => item.status == "incomplete");
+
+        setTodoListData(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <AddToDoForm fetchData={fetchData} />
+      {toDoListData && <ListView toDoListData={toDoListData} fetchData={fetchData} />}
+    </div>
+  );
+};
+
+export default ToDoApp;
